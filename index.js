690066864423cc0106153361bc35be96b5bf49e2
ATOROYO <davidatoroyosika@gmail.com>
2024-09-07 23:50:54 +0300

    Finished Challenge 4 for part 1

--- index.js
+++ index.js
@@ -37,3 +37,12 @@ if (dolphins >= 100 && dolphins > koalas) {
 } else if (dolphins >= 100 && dolphins === koalas) {
   console.log(`That is a draw`);
 }
+
+// Coding Challenge #4
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+// 300. If the value is different, the tip is 20%.
+const bill = 30;
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+${tip + bill}`);
