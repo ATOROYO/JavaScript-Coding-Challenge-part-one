95128b8e0ddbdac96ef0895222971025a32ebc36
ATOROYO <davidatoroyosika@gmail.com>
2024-09-07 22:49:59 +0300

    Calculated compared Mark's and John's BMIs

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const markBMI = markWeight / (markHeight * markHeight);
 const johnBMI = johnWeight / (johnHeight * johnHeight);
 
 // Comparing their BMIs
-const markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+if (markBMI > johnBMI) {
+  console.log(`Mark's
+BMI (${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+  console.log(`John's
+        BMI (${johnBMI}) is higher than Mark's (${markBMI})`);
+}
